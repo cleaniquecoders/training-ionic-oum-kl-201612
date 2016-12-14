9315a8c228080bdf5afafb75c40f37ead0d9939b
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-12-15 05:59:01 +0800

    basic get current GPS location

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,19 @@
 angular.module('app.controllers', [])
   
-.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams', '$cordovaGeolocation',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
+function ($scope, $stateParams, $cordovaGeolocation) {
+	console.log('hi');
+	var posOptions = {timeout: 10000, enableHighAccuracy: false};
+	  $cordovaGeolocation
+	    .getCurrentPosition(posOptions)
+	    .then(function (position) {
+	      var lat  = position.coords.latitude
+	      var long = position.coords.longitude
+	      alert('You current latitude is ' + lat + ' and your longitude is ' + long);
+	    }, function(err) {
+	      // error
+	    });
 }])
  
